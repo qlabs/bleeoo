6bd51c31f5845430540b120af1516d2ee13dd770
Eric Skiff <eric@dropio.com>
2011-06-21 17:48:21 -0400

    generate the UUID for that there video

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,8 @@
 class WelcomeController < ApplicationController
   # GET /
   def index
-   @videos = Video.paginate :page => (params[:page] || 1) , :order => 'created_at DESC', :per_page => 4
+    @video_uid = UUID.generate    
+    @videos = Video.paginate :page => (params[:page] || 1) , :order => 'created_at DESC', :per_page => 4
   end
 
 end
